18171166d205cdcc764ca6d10a43f92dbcf1f02c
github <71609662+areengithub@users.noreply.github.com>
2021-01-05 22:43:05 +0300

    Areen UP
    Areen UP

--- aroo.js
+++ aroo.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['id owner']
+const myid = ['id wner']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
